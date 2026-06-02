d09331195e58cd2ae218eecd75eda599639660a1
feliaffonso30-prog <feliaffonso30@gmail.com>
2026-06-02 19:26:35 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,20 @@ import { HowItWorks } from "@/components/landing/how-it-works"
 import { Differentiators } from "@/components/landing/differentiators"
 import { CTA } from "@/components/landing/cta"
 import { Footer } from "@/components/landing/footer"
+import { Waitlist } from "@/components/Waitlist"
 
 export default function Page() {
   return (
     <>
       <Header />
       <main>
-        <Hero />
-        <Problem />
-        <HowItWorks />
-        <Differentiators />
-        <CTA />
-      </main>
+  <Hero />
+  <Problem />
+  <HowItWorks />
+  <Differentiators />
+  <CTA />
+  <Waitlist />
+</main>
       <Footer />
     </>
   )
